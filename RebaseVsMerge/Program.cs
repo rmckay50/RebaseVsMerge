03d4aae232a5b56d94ff03abb70fd95141363350
Rod <rod.mckay49@gmail.com>
2023-04-10 14:30:39 -0600

    4th main

--- RebaseVsMerge/Program.cs
+++ RebaseVsMerge/Program.cs
@@ -13,6 +13,7 @@ namespace RebaseVsMerge
             Console.WriteLine("first commit in main");
             Console.WriteLine("second commit in main");
             Console.WriteLine("3rd main");
+            Console.WriteLine("4th main");
             Console.ReadLine();
         }
     }
